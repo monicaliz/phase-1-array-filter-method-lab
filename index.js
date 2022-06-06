76d57c1276464a55e899fb573a6a63cdc04194f6
monicaliz <monicaeliz3@gmail.com>
2022-06-05 20:05:36 -0400

    commit tree 9c458cb12ed821c1fee3569ccea61a38d92a1d54

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+//findMatching
+
+function findMatching(drivers, name) {
+    return drivers.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, members) {
+    return drivers.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(members.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter((record) => record.name === name)
+  }
